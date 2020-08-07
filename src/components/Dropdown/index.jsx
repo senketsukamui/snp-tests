272eacc3b5932610c664c3b814ec9c88a5d33ccc
Gleb Potapov <glebpotapov2001@gmail.com>
2020-08-07 20:48:21 +0300

    Make dropdown close by outside click

--- src/components/Dropdown/index.jsx
+++ src/components/Dropdown/index.jsx
@@ -5,6 +5,8 @@ import down_arrow from 'assets/images/down_arrow.png';
 const Dropdown = props => {
   const [open, setOpen] = React.useState(false);
   const [selected, changeSelected] = React.useState(props.items[0]);
+  const ref = React.useRef(null);
+
   const handleItemClick = e => {
     changeSelected(e.target.dataset.type);
     props.handleChange(e.target.dataset.type);
@@ -13,8 +15,19 @@ const Dropdown = props => {
   const handleDropdownClick = () => {
     setOpen(!open);
   };
+  React.useEffect(() => {
+    const handleClickOutside = e => {
+      if (ref.current && !ref.current.contains(event.target)) {
+        setOpen(false);
+      }
+    };
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  });
   return (
-    <div className={styles.wrapper}>
+    <div className={styles.wrapper} ref={ref}>
       <div className={styles.selection} onClick={handleDropdownClick}>
         {selected}
         {!open && <img src={down_arrow} alt="" className={styles.down_arrow} />}
